b894b9d72ca9d426372b540e443a00ab257b0903
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-10-08 16:09:02 +0300

    add sample text to home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod fugiat rerum ex minus, assumenda earum ea natus aliquid dignissimos modi, iste in facilis unde magni dolorum officiis, est suscipit cum.</div>
+    )
+}
+
+export default Home;
