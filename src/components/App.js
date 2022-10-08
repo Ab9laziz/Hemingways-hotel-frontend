0c03b2c71e7c61d69ed3a151b1c5fe788653ca0c
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-10-08 16:05:55 +0300

    add afew route for the different nav limks

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
-import '../css/App.css'
 import Home from './Home'
+import {Route, Switch} from 'react-router-dom'
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Subscribe from './Subscribe';
+import Feedback from './Feedback';
+
+
+
 function App() {
   return (
       <>
-      <Home/>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/feedback">
+          <Feedback/>
+        </Route>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+      </Switch>
+      <Subscribe/>
+      <Footer/>
       </>
   );
 }
