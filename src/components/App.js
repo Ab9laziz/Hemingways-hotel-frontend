2a83262812f6c2ba615843fadc66494a81e50cfc
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-10-10 00:02:35 +0300

    create routes for the different components

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import Home from './Home'
 import {Route, Switch} from 'react-router-dom'
 import Navbar from './Navbar';
 import Footer from './Footer';
-import Subscribe from './Subscribe';
 import Feedback from './Feedback';
-
+import Book from './Book';
+import Subscribe from './Subscribe';
+import Offers from './Offers';
 
 
 function App() {
@@ -15,11 +16,19 @@ function App() {
         <Route exact path="/feedback">
           <Feedback/>
         </Route>
+        <Route exact path="/book">
+          <Book/>
+        </Route>
+        <Route exact path="/contact">
+          <Subscribe/>
+        </Route>
+        <Route exact path="/offers">
+          <Offers/>
+        </Route>
         <Route exact path="/">
           <Home/>
         </Route>
       </Switch>
-      <Subscribe/>
       <Footer/>
       </>
   );
