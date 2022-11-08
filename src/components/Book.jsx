07950f9a809ca60e68108cc60f77b37dd18746fe
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-11-08 14:18:03 +0300

    change firstname and lastname to Name

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,9 +4,9 @@ import "../css/style.css"
 
 
 function Book(){
-const[formData, setValue] =useState({
-    fname:'',
-    lname:'',
+const[formData, setValue] = useState({
+    name:'',
+    // lname:'',
     email:'',
     number:'',
     type:'executive_suites'
@@ -45,10 +45,10 @@ console.log(formData)
             <h2>Fill the form below to kick start your stay with us</h2>
             <div className="cont4">
                 <form onSubmit={handleSubmit} >
-                    <label htmlFor="fname">First name : </label>
-                    <input required type="text" name="fname" id="fname" onChange={handleChange} value={formData.fname} placeholder="Enter your first name"/><br /><br />
-                    <label htmlFor="lname">Last name : </label>
-                    <input required type="text" name="lname" id="lname" onChange={handleChange} value={formData.lname} placeholder="Enter your last name"/><br /><br />
+                    <label htmlFor="fname">Name : </label>
+                    <input required type="text" name="Name" id="name1" onChange={handleChange} value={formData.name} placeholder="Enter your first name"/><br /><br />
+                    {/* <label htmlFor="lname">Last name : </label>
+                    <input required type="text" name="lname" id="lname" onChange={handleChange} value={formData.lname} placeholder="Enter your last name"/><br /><br /> */}
                     <label htmlFor="email">Email : </label>
                     <input classname="email4"required type="email" name="email" id="email" onChange={handleChange} value={formData.email} placeholder="Enter your email" /><br /><br />
                     <label htmlFor="number">Phone number : </label>
@@ -56,7 +56,7 @@ console.log(formData)
                     <label htmlFor="type">Choose the suite you would like : </label> 
                     <select id='type' onChange={handleChange} value={formData.type} >
                         <option value="executive_suites">The Executive Suites</option>
-                        <option value="himingway_suites">The Hemingway Suites</option>
+                        <option value="hemingway_suites">The Hemingway Suites</option>
                         <option value="deluxe_suites">Deluxe Suites</option>
                         <option value="blixen_suites">Blixen Suites</option>
                     </select><br />
